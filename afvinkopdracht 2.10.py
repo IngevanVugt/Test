1f472712ec8e067f0dbdb996cd63c6599cf50f44
IngevanVugt <43170087+IngevanVugt@users.noreply.github.com>
2018-09-14 12:26:09 +0200

    Add files via upload

---
+++ afvinkopdracht 2.10.py
@@ -0,0 +1,16 @@
+een_cookie_suiker = 1.5 / 48
+een_cookie_boter = 1 / 48
+een_cookie_meel = 2.75 / 48
+
+
+hoeveel_cookies = float(input("hoeveel cookies wil je bakken "))
+
+Suiker = een_cookie_suiker * hoeveel_cookies
+Boter = een_cookie_boter * hoeveel_cookies
+Meel = een_cookie_meel * hoeveel_cookies
+
+print("aantal bekers of suiker:", Suiker)
+print("aantal bekers of boter:", Boter)
+print("aantal bekers of meel:", Meel)
+
+
